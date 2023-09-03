c09ebf16a3fa5640bb24d61fe96b97a86a9e56c8
Lucas-Lopes-II <lucas.santos.pessoal@outlook.com>
2023-09-03 12:48:05 -0300

    feat: creation of the users module's initial structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { UsersModule } from './modules/users/infrastructure/users.module';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 
 @Module({
-  imports: [EnvConfigModule],
+  imports: [EnvConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
