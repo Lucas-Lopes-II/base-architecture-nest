9c532888fa63e27e47a8103350e48f6b94f6c11e
Lucas-Lopes-II <lucas.santos.pessoal@outlook.com>
2023-09-03 13:04:22 -0300

    test: UserEntity unit tests - constructor method

--- src/modules/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/modules/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,1 +1,23 @@
+import { faker } from '@faker-js/faker';
+import { UserEntity, UserProps } from '../../user.entity';
 
+describe('UserEntity unit tests', () => {
+  let props: UserProps;
+  let userEntity: UserEntity;
+
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+    userEntity = new UserEntity(props);
+  });
+
+  it('Constructor method', () => {
+    expect(userEntity.props.name).toEqual(props.name);
+    expect(userEntity.props.email).toEqual(props.email);
+    expect(userEntity.props.password).toEqual(props.password);
+    expect(userEntity.props.createdAt).toBeInstanceOf(Date);
+  });
+});
