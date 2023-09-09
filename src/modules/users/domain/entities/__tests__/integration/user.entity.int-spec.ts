8805b91c6d2959850f257563805b703720809263
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2023-09-09 10:31:44 -0300

    test: UserEntity and EntityValidationError integration tests - field name

---
+++ src/modules/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -0,0 +1,27 @@
+import { UserEntity, UserProps } from '../../user.entity';
+import { UserDataBuilder } from '../../testing/helpers/user-data-builder';
+import { EntityValidationError } from './../../../../../../shared/domain/errors/validation-error';
+
+describe('UserEntity integration tests', () => {
+  describe('Constructor method', () => {
+    it('Should throw an error when creating a user with invalid name', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        name: null,
+      };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        name: '',
+      };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        name: 'a'.repeat(256),
+      };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+    });
+  });
+});
