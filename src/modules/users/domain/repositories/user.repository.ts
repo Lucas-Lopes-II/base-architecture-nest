5dd32b9c0cf968bb7c2221e2c7ddbe3093f697b7
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2023-09-10 18:57:55 -0300

    style: change of nomenclatures

--- src/modules/users/domain/repositories/user.repository.ts
+++ src/modules/users/domain/repositories/user.repository.ts
@@ -1,7 +1,7 @@
 import { UserEntity } from '../entities/user.entity';
 import { ISearchableRepository } from './../../../../shared/domain/repositories/searchable-repository.interface';
 
-export interface UserRepository
+export interface IUserRepository
   extends ISearchableRepository<UserEntity, any, any> {
   findByEmail(email: string): Promise<UserEntity>;
   emailExists(email: string): Promise<void>;
--- src/modules/users/infrastructure/database/repositories/in-memory/user-in-memory.repository.ts
+++ src/modules/users/infrastructure/database/repositories/in-memory/user-in-memory.repository.ts
@@ -1,12 +1,12 @@
 import { UserEntity } from '../../../../domain/entities/user.entity';
-import { UserRepository } from '../../../../domain/repositories/user.repository';
+import { IUserRepository } from '../../../../domain/repositories/user.repository';
 import { ConflictError } from '../../../../../../shared/domain/errors/conflict-error';
 import { NotFoundError } from '../../../../../../shared/domain/errors/not-found-error';
 import { InMemorySearchableRepository } from '../../../../../../shared/domain/repositories/in-memory-searchable.repository';
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
-  implements UserRepository
+  implements IUserRepository
 {
   public async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find((item) => item.email === email);
